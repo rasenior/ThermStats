1e3b69d23654efc7f76cac880dec1bb8daba78cc
Rebecca Senior <rebecca.a.senior@gmail.com>
2018-05-22 16:21:28 +0100

    Adding a Worldclim2 dataset (January mean temp for Sulawesi)
    Signed-off-by: Rebecca Senior <rebecca.a.senior@gmail.com>

---
+++ R/worldclim_data.R
@@ -0,0 +1,18 @@
+#' WorldClim2 mean monthly temperature data for Sulawesi
+#'
+#' Gridded temperature data at 30 arc-second resolution for the island of
+#' Sulawesi. Temperature is average January temperature (°C) for the 'current'
+#' time period (1970-2000).
+#'
+#' @docType data
+#' @usage worldclim_sulawesi
+#' @format A geographic temperature raster
+#' @keywords datasets
+#' @references Fick, S.E. and Hijmans, R.J. (2017),
+#' Worldclim 2: New 1-km spatial resolution climate surfaces for global land
+#' areas. International Journal of Climatology. Available at:
+#' \url{http://worldclim.org/version2}.
+#' @examples
+#' plot(worldclim_sulawesi)
+#' hist(plot(worldclim_sulawesi))
+"worldclim_sulawesi"
