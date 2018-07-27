4e471461a21a349eb735c7db07a4d874c6a3f654
Rebecca A. Senior <rebecca.a.senior@gmail.com>
2018-07-27 15:39:24 +0100

    Few more minor tweaks to vignette text & formatting
    Signed-off-by: Rebecca A. Senior <rebecca.a.senior@gmail.com>

--- inst/doc/thermstats_vignette.R
+++ inst/doc/thermstats_vignette.R
@@ -2,19 +2,16 @@
 knitr::opts_chunk$set(echo = FALSE, message = FALSE, warning = FALSE,
                       fig.align = "centre", fig.width = 16.6/2.54)
 
-## ----install, echo = TRUE, eval = -1-------------------------------------
+## ----install, echo = TRUE------------------------------------------------
 devtools::install_github("rasenior/ThermStats")
 library(ThermStats)
 
 ## ----batch-extract, echo= TRUE, results = "hide"-------------------------
-# Batch extract four FLIR thermal images included 
-# in the ThermStats package installation
+# Batch extract thermal images included in ThermStats installation
 flir_raw <-
-   batch_extract(in_dir = 
-                     system.file("extdata",
-                                 package = 
-                                     "ThermStats"),
-                 write_results = FALSE)
+    batch_extract(in_dir = system.file("extdata", 
+                                       package = "ThermStats"),
+                  write_results = FALSE)
 
 ## ----batch-convert, echo= TRUE, results = "hide"-------------------------
 # Define raw data
@@ -24,39 +21,29 @@ camera_params <- flir_raw$camera_params
 # Define metadata
 metadata <- flir_metadata
 
-# Create vector denoting the position of each 
-# photo within the metadata dataframe
+# Create vector denoting the position of each photo within metadata
 photo_index <- match(names(raw_dat), 
                      metadata$photo_no)
 
-# Batch convert ---------------------------------
+# Batch convert -----------------------------------------------------------------------------------
 flir_converted <-
     batch_convert(
         raw_dat = raw_dat,
-        # Emissivity = mean of the range in 
-        # Scheffers et al. 2017
+        # Emissivity = mean of the range in Scheffers et al. 2017
         E = mean(c(0.982,0.99)),
-        # Object distance = hypotenuse of a right
-        # triangle where the vertical side is 
-        # 1.3 m (breast height) & the angle down 
-        # is 45 degrees
+        # Object distance = hypotenuse of a right triangle where the vertical side is 1.3 m (breast 
+        # height) & the angle down is 45 degrees
         OD = (sqrt(2))*1.3,
-        # Apparent reflected temperature, 
-        # atmospheric temperature and infrared 
-        # window temperature set as the 
-        # atmospheric temperature measured in 
-        # the field
+        # Apparent reflected temperature, atmospheric temperature & infrared window temperature =
+        # atmospheric temperature measured in the field
         RTemp = metadata$atm_temp[photo_index],
         ATemp = metadata$atm_temp[photo_index],
         IRWTemp = metadata$atm_temp[photo_index],
-        # Infrared Window transmission kept at 
-        # default value of 1
+        # Infrared Window transmission = default value of 1
         IRT = 1,
-        # Relative humidity is set as the 
-        # relative humidity measured in the field
+        # Relative humidity = relative humidity measured in the field
         RH = metadata$rel_humidity[photo_index],
-        # Calibration constants from 
-        # batch_extract, constant for each camera
+        # Calibration constants from 'batch_extract'
         PR1 = camera_params[,"PlanckR1"],
         PB = camera_params[,"PlanckB"],
         PF = camera_params[,"PlanckF"],
@@ -72,28 +59,27 @@ flir_stats <-
         val_mat = flir_converted$`8565`,
         # The ID of the matrix
         matrix_id = "8565",
-        # Whether or not to identify hot and 
-        # cold spots
+        # Whether or not to identify hot and cold spots
         get_patches = TRUE,
-        # The size of the neighourhood 
-        # (for calculating local G statistic)
+        # The size of the neighourhood (for calculating local G stat)
         k = 8,
-        # The neghbour weighting style 
-        # (for calculating local G statistic)
+        # The neghbour weighting style (for calculating local G stat)
         style = "W",
-        # The matrix projection
-        # (only relevant for geographic data)
+        # The matrix projection (only relevant for geographic data)
         mat_proj = NULL,
-        # The matrix extent
-        # (only relevant for geographic data)
+        # The matrix extent (only relevant for geographic data)
         mat_extent = NULL,
         # The data to return
-        return_vals = c("df", 
-                        "patches", 
-                        "pstats"),
-        # The names of the statistics functions
+        return_vals = c(
+            # Temperature data as dataframe
+            "df", 
+            # SpatialPolygonsDataFrame of patch outlines
+            "patches", 
+            # Patch statistics dataframe
+            "pstats"),
+        # The names of the statistics functions (to manually name columns in the 'pstats' dataframe)
         pixel_fns = NULL,
-        # The summary statistics
+        # The summary statistics of interest
         median, perc_5, perc_95, SHDI
     )
 
@@ -124,15 +110,13 @@ flir_stats_group <-
         grouping_var = "rep_id",
         # Desired precision of data
         round_val = 0.5,
-        # The size of the neighourhood 
-        # (for calculating local G statistic)
+        # The size of the neighourhood (for calculating local G stat)
         k = 8,
-        # The neghbour weighting style 
-        # (for calculating local G statistic)
+        # The neghbour weighting style (for calculating local G stat)
         style = "W",
-        # Some summary statistics
+        # The summary statistics of interest
         median, perc_5, perc_95, SHDI
-        )
+    )
 
 ## ----tab-B-2, results='asis'---------------------------------------------
 tab2 <- "
@@ -146,11 +130,11 @@ Table: A snippet of hot spot patch statistics returned by `stats_by_group`, whic
 
 cat(tab2) # output the table in a format good for HTML/PDF/docx conversion
 
-## ----fig-B-1, fig.cap= "The output of 'plot_patches' includes a histogram and the original temperature data overlaid with outlines of hot and cold spots, identified using the Getis-Ord local statistic."----
+## ----fig-B-1, fig.cap= "The output of `plot_patches` includes a histogram and the original temperature data overlaid with outlines of hot and cold spots, identified using the Getis-Ord local statistic.", echo = TRUE----
 plot_patches(
     # The raw temperature data
     df = flir_stats$df,
     # The patch outlines
     patches = flir_stats$patches
-        )
+)
 
