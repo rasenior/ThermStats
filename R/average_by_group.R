cb9a37803c02345918830e58c8a2335b8a1a253a
Rebecca A. Senior <rebecca.a.senior@gmail.com>
2018-11-15 10:54:19 +0000

    Making sure works for raster stacks as well as matrix list, and adding example to documentation
    Signed-off-by: Rebecca A. Senior <rebecca.a.senior@gmail.com>

--- R/average_by_group.R
+++ R/average_by_group.R
@@ -2,24 +2,66 @@
 #'
 #' Average multiple matrices or rasters within groups.
 #' @param metadata A dataframe denoting the grouping of different matrices.
-#' @param mat_list List of matrices.
+#' @param mat_list List of matrices or raster stack.
 #' @param matrix_id Name of the metadata variable that identifies unique
 #' matrices. Should match element names in the list of matrices.
-#' @param k Number of neighbours to use when calculating nearest neighbours
-#' using \code{spdep::}\code{\link[spdep]{knearneigh}}.
-#' @param style Style to use when calculating neighbourhood weights using
-#'  \code{spdep::}\code{\link[spdep]{nb2listw}}.
 #' @param grouping_var The name of the metadata variable that denotes the
 #' grouping of matrices.
-#' @param round_val Value to round to. Use 1 for no rounding.
-#' @param ... Use to specify summary statistics that should be calculated across
-#' all pixels. Several helper functions are included for use here:
-#' \code{\link{perc_5}}, \code{\link{perc_95}},
-#' \code{\link{SHDI}}, \code{\link{SIDI}},
-#' \code{\link{kurtosis}} and \code{\link{skewness}}. Also see examples below.
-#' @return A dataframe with pixel and patch statistics. See
-#' \code{\link{patch_stats}} for details of all the patch statistics returned.
+#' @param round_val Number of decimals to round average matrices to (optional).
+#' @return 
+#' If \code{mat_list} is supplied as a list of matrices, returns
+#' a new list of matrices with one element for each unique value of the grouping
+#' variable. 
+#' 
+#' If \code{mat_list} is supplied as a raster stack, returns a new raster
+#' stack with one raster for each unique value of the grouping variable. 
+#' 
+#' Grouping variables with no matching matrices are dropped.
 #' @examples
+#' # Load raw data
+#' raw_dat <- flir_raw$raw_dat
+#' camera_params <- flir_raw$camera_params
+#' metadata <- flir_metadata
+#'
+#' # Batch convert
+#' mat_list <- batch_convert(raw_dat, write_results = FALSE)
+#' 
+#' # With list of matrices -----------------------------------------------------
+#' 
+#' # Average matrices in list
+#' avg_list <- average_by_group(metadata = metadata,
+#'                              mat_list = mat_list,
+#'                              matrix_id = "photo_no",
+#'                              grouping_var = "rep_id")
+#'                              
+#' # With raster stack ---------------------------------------------------------
+#' 
+#' # Coerce each matrix to a raster
+#' mat_stack <-
+#'     lapply(mat_list,
+#'            function(x) raster::raster(x,
+#'                                       xmn=0, xmx=ncol(mat_list[[1]]),
+#'                                       ymn=0, ymx=nrow(mat_list[[1]])))
+#' # Stack
+#' mat_stack <- raster::stack(mat_stack)
+#' 
+#' # Average matrices in raster stack
+#' avg_stack <- average_by_group(metadata = metadata,
+#'                               mat_list = mat_stack,
+#'                               matrix_id = "photo_no",
+#'                               grouping_var = "rep_id")
+#' # Plot
+#' raster::plot(avg_stack)
+#' 
+#' # Compare to invididual pics
+#' # Photos 8565 and 8583 were averaged to make T7P1
+#' # Photos 8589 and 8613 were averaged to make T7P2
+#' 
+#' allstack <- raster::stack(mat_stack, avg_stack)
+#' rasterVis::levelplot(allstack,
+#'                     layout=c(2, 3),
+#'                     index.cond=list(c(1, 3, 2, 4, 5, 6)))
+#'                     
 #' @export
 #'
 # Define function to return stats for each grouping
@@ -27,25 +69,26 @@ average_by_group <- function(metadata,
                              mat_list,
                              matrix_id,
                              grouping_var,
-                             round_val){
+                             round_val = NULL){
     
-    # Subset raster stack
-    substack <- subset_stack(metadata = metadata, 
-                             mat_list = mat_list,
-                             matrix_id = matrix_id,
-                             grouping_var = grouping_var, 
-                             grouping_val = "PT1_1_0_1_1")
+    # Determine if raster stack or list of matrices
+    if(class(mat_list)[1] == "RasterStack"){
+        mat_type <- "rasters"
+    }else mat_type <- "matrices"
+    
     if (requireNamespace("pbapply", quietly = TRUE)) {
         mat_avgs <-
             pbapply::pblapply(unique(metadata[, grouping_var]),
                               function(x){
                                   tryCatch({
                                       # Calculate mean for each group
-                                      group_avg <- avg_stack(metadata = metadata, 
-                                                             mat_list = mat_list,
-                                                             matrix_id = matrix_id,
-                                                             grouping_var = grouping_var, 
-                                                             grouping_val = x)
+                                      group_avg <- avg_sub(metadata = metadata, 
+                                                           mat_list = mat_list,
+                                                           matrix_id = matrix_id,
+                                                           grouping_var = grouping_var, 
+                                                           grouping_val = x,
+                                                           mat_type = mat_type,
+                                                           round_val = round_val)
                                       
                                       # Return
                                       return(group_avg)
@@ -59,17 +102,19 @@ average_by_group <- function(metadata,
                                       
                                   })
                               })
+        
     }else{
         mat_avgs <-
             lapply(unique(metadata[, grouping_var]),
                    function(x){
                        tryCatch({
                            # Calculate mean for each group
-                           group_avg <- avg_stack(metadata = metadata, 
-                                                  mat_list = mat_list,
-                                                  matrix_id = matrix_id,
-                                                  grouping_var = grouping_var, 
-                                                  grouping_val = x)
+                           group_avg <- avg_sub(metadata = metadata, 
+                                                mat_list = mat_list,
+                                                matrix_id = matrix_id,
+                                                grouping_var = grouping_var, 
+                                                grouping_val = x,
+                                                mat_type = mat_type)
                            
                            # Return
                            return(group_avg)
@@ -82,17 +127,31 @@ average_by_group <- function(metadata,
                            return(NA)
                            
                        })
-                   })
+                   })   
     }
     
+    if(mat_type == "rasters"){
+        # Drop NAs
+        mat_avgs <- mat_avgs[!(is.na(mat_avgs))]
+        # Stack
+        mat_avgs <- raster::stack(mat_avgs)
+    }else{
+        # Name
+        names(mat_avgs) <- unique(metadata[, grouping_var])
+        # Drop NAs
+        mat_avgs <- mat_avgs[!(is.na(mat_avgs))]
+    }
     
+    return(mat_avgs)
 }
 
-avg_stack <- function(metadata,
-                      mat_list,
-                      matrix_id,
-                      grouping_var,
-                      grouping_val){
+avg_sub <- function(metadata,
+                    mat_list,
+                    matrix_id,
+                    grouping_var,
+                    grouping_val,
+                    mat_type,
+                    round_val = NULL){
     
     # Define matrix IDs for the group
     ids <- metadata[metadata[,grouping_var] == grouping_val, matrix_id]
@@ -100,11 +159,19 @@ avg_stack <- function(metadata,
     # Define the indices of these matrices in the matrix list
     inds <- which(gsub("[a-z]","",tolower(names(mat_list))) %in% ids)
     
-    if(class(mat_list)[1] == "RasterStack"){
+    # If there are no matches, return NA
+    if(length(inds) == 0){
+        # Return
+        return(NA) 
+    }
+    
+    if(mat_type == "rasters"){
         # Subset
         substack <- mat_list[[inds]]
         # Create mean raster
         avg <- raster::mean(substack)
+        # If rounding desired, round here
+        if(!(is.null(round_val))) avg <- round(avg, digits = round_val)
         # Name it
         names(avg) <- grouping_val
     }else{
@@ -112,6 +179,8 @@ avg_stack <- function(metadata,
         substack <- mat_list[inds]
         # Create mean matrix
         avg <- apply(simplify2array(substack), 1:2, mean)
+        # If rounding desired, round here
+        if(!(is.null(round_val))) avg <- round(avg, digits = round_val)
     }
     
     # Return
