63cf20d346de7f3f93ed52f9adf8c5eff519d472
Rebecca A. Senior <rebecca.a.senior@gmail.com>
2018-11-17 11:32:06 +0000

    Removing nearest neighbour as an argument, removing dots as way to supply summary stats, improving subsetting function
    Signed-off-by: Rebecca A. Senior <rebecca.a.senior@gmail.com>

--- R/stats_by_group.R
+++ R/stats_by_group.R
@@ -37,8 +37,7 @@
 #'                    matrix_id = "photo_no",
 #'                    grouping_var = "rep_id",
 #'                    round_val = 0.5,
-#'                    k = 8,
-#'                    style = "W",
+#'                    style = "C",
 #'                    mean, max, min)
 #'
 #' # Pixel stats = kurtosis and sknewness
@@ -48,8 +47,7 @@
 #'                    matrix_id = "photo_no",
 #'                    grouping_var = "rep_id",
 #'                    round_val = 0.5,
-#'                    k = 8,
-#'                    style = "W",
+#'                    style = "C",
 #'                    kurtosis, skewness)
 #'
 #' # Pixel stats = 5th and 95th percentiles
@@ -59,8 +57,7 @@
 #'                    matrix_id = "photo_no",
 #'                    grouping_var = "rep_id",
 #'                    round_val = 0.5,
-#'                    k = 8,
-#'                    style = "W",
+#'                    style = "C",
 #'                    perc_5, perc_95)
 #'
 #' # Pixel stats = Shannon and Simpson Diversity Indices
@@ -70,8 +67,7 @@
 #'                    matrix_id = "photo_no",
 #'                    grouping_var = "rep_id",
 #'                    round_val = 0.5,
-#'                    k = 8,
-#'                    style = "W",
+#'                    style = "C",
 #'                    SHDI, SIDI)
 #' @export
 #'
@@ -79,97 +75,103 @@
 stats_by_group <- function(metadata,
                            mat_list,
                            matrix_id,
-                           k = 8,
-                           style = "W",
+                           style = "C",
                            grouping_var,
-                           round_val,...){
-  pixel_fns <- paste(match.call(expand.dots = FALSE)$...)
-
-  if (requireNamespace("pbapply", quietly = TRUE)) {
-    temp_stats<-
-      pbapply::pblapply(unique(metadata[, grouping_var]),
-                        function(x){
-                          tryCatch({
-                            sub_mat <- create_subset(metadata = metadata,
-                                                     mat_list = mat_list,
-                                                     matrix_id = matrix_id,
-                                                     grouping_var = grouping_var,
-                                                     grouping_val = x,
-                                                     round_val = round_val)
-                            n_mat <- sub_mat[["n_mat"]]
-                            sub_mat <- sub_mat[["sub_mat"]]
-
-                            # Get stats
-                            result <-
-                              get_stats(val_mat = sub_mat,
-                                        matrix_id = x,
-                                        get_patches = TRUE,
-                                        k = k,
-                                        style = style,
-                                        mat_proj = NULL,
-                                        mat_extent = NULL,
-                                        return_vals = "pstats",
-                                        pixel_fns = pixel_fns,
-                                        ...
-                              )
-                            # Add number matrices
-                            result <- cbind(result, n_mat)
-                            # Return
-                            return(result)
-
-                          },
-                          error = function(err) {
-
-                            # error handler picks up where error was generated
-                            message(paste("\nMY_ERROR:  ",err))
-                            return(NA)
-
-                          })
-                        })
-  }else{
-    temp_stats<-
-      lapply(unique(metadata[, grouping_var]),
-             function(x){
-               tryCatch({
-                 sub_mat <- create_subset(metadata = metadata,
-                                          mat_list = mat_list,
-                                          matrix_id = matrix_id,
-                                          grouping_var = grouping_var,
-                                          grouping_val = x,
-                                          round_val = round_val)
-                 n_mat <- sub_mat[["n_mat"]]
-                 sub_mat <- sub_mat[["sub_mat"]]
-
-                 # Get stats
-                 result <-
-                   get_stats(val_mat = sub_mat,
-                             matrix_id = x,
-                             get_patches = TRUE,
-                             k = k,
-                             style = style,
-                             mat_proj = NULL,
-                             mat_extent = NULL,
-                             return_vals = "pstats",
-                             pixel_fns = pixel_fns,
-                             ...
-                   )
-                 # Add number matrices
-                 result <- cbind(result, n_mat)
-                 # Return
-                 return(result)
-
-               },
-               error = function(err) {
-
-                 # error handler picks up where error was generated
-                 message(paste("\nMY_ERROR:  ",err))
-                 return(NA)
-
-               })
-             })
-  }
-  temp_stats <- do.call("rbind",temp_stats)
-  return(temp_stats)
+                           round_val,
+                           sum_stats = c("mean", "min","max")){
+    
+    # Determine if raster stack or list of matrices
+    if(class(mat_list)[1] == "RasterStack"){
+        mat_type <- "rasters"
+    }else mat_type <- "matrices"
+    
+    if (requireNamespace("pbapply", quietly = TRUE)) {
+        temp_stats<-
+            pbapply::pblapply(unique(metadata[, grouping_var]),
+                              function(x){
+                                  tryCatch({
+                                      sub_mat <- create_subset(metadata = metadata,
+                                                               mat_list = mat_list,
+                                                               matrix_id = matrix_id,
+                                                               grouping_var = grouping_var,
+                                                               grouping_val = x,
+                                                               mat_type = mat_type,
+                                                               round_val = round_val)
+                                      n_mat <- sub_mat[["n_mat"]]
+                                      sub_mat <- sub_mat[["sub_mat"]]
+                                      
+                                      # Get stats
+                                      result <-
+                                          get_stats(val_mat = sub_mat,
+                                                    matrix_id = x,
+                                                    calc_connectivity = FALSE,
+                                                    conn_threshold = NULL,
+                                                    get_patches = TRUE,
+                                                    style = style,
+                                                    mat_proj = NULL,
+                                                    mat_extent = NULL,
+                                                    return_vals = "pstats",
+                                                    sum_stats = sum_stats
+                                          )
+                                      
+                                      # Add number matrices
+                                      result <- cbind(result, n_mat)
+                                      # Return
+                                      return(result)
+                                      
+                                  },
+                                  error = function(err) {
+                                      
+                                      # error handler picks up where error was generated
+                                      message(paste("\nMY_ERROR:  ",err))
+                                      return(NA)
+                                      
+                                  })
+                              })
+    }else{
+        temp_stats<-
+            lapply(unique(metadata[, grouping_var]),
+                   function(x){
+                       tryCatch({
+                           sub_mat <- create_subset(metadata = metadata,
+                                                    mat_list = mat_list,
+                                                    matrix_id = matrix_id,
+                                                    grouping_var = grouping_var,
+                                                    grouping_val = x,
+                                                    round_val = round_val)
+                           n_mat <- sub_mat[["n_mat"]]
+                           sub_mat <- sub_mat[["sub_mat"]]
+                           
+                           # Get stats
+                           result <-
+                               get_stats(val_mat = sub_mat,
+                                         matrix_id = x,
+                                         get_patches = TRUE,
+                                         k = k,
+                                         style = style,
+                                         mat_proj = NULL,
+                                         mat_extent = NULL,
+                                         return_vals = "pstats",
+                                         pixel_fns = pixel_fns,
+                                         ...
+                               )
+                           # Add number matrices
+                           result <- cbind(result, n_mat)
+                           # Return
+                           return(result)
+                           
+                       },
+                       error = function(err) {
+                           
+                           # error handler picks up where error was generated
+                           message(paste("\nMY_ERROR:  ",err))
+                           return(NA)
+                           
+                       })
+                   })
+    }
+    temp_stats <- do.call("rbind",temp_stats)
+    return(temp_stats)
 }
 
 
@@ -178,50 +180,64 @@ create_subset <- function(metadata,
                           matrix_id,
                           grouping_var,
                           grouping_val,
-                          round_val = round_val){
-  # Define the identifiers for the matrices that fall within each group
-  ids <-
-    as.character(metadata[metadata[,grouping_var]==grouping_val,matrix_id])
-  # Define the number of matrices  in this group
-  n_mat <- length(ids)
-
-  # Subset matrices list by the desired matrix IDs
-  sub_list <- mat_list[as.character(ids)]
-
-  # Coerce any raster layers to matrices
-  raster_list <-
-    sub_list[which(sapply(sub_list, function(x) class(x) == "RasterLayer"))]
-
-  if(length(raster_list) > 0){
+                          mat_type,
+                          round_val){
+    
+    # Define matrix IDs for the group
+    ids <- unique(metadata[metadata[,grouping_var] == grouping_val, matrix_id])
+    
+    # If raster stack need to paste 'X' onto numeric ids
+    if(mat_type == "rasters" & is.numeric(ids)) ids <- paste("X", ids, sep = "")
+    
+    # Define the indices of these matrices in the matrix list
+    inds <- which(names(mat_list) %in% ids)
+    
+    # If there are no matches, return NA
+    if(length(inds) == 0){
+        # Return
+        return(NA) 
+    }
+    
+    # Define the number of matrices  in this group
+    n_mat <- length(ids)
+    
+    # Subset matrices list by the desired matrix IDs
+    sub_list <- mat_list[inds]
+    
+    # Coerce any raster layers to matrices
     raster_list <-
-      lapply(raster_list, function(rast){
-        # To dataframe first (seems to preserve coordinates more reliably)
-        temp_df <- raster::as.data.frame(rast, xy = TRUE)
-        # To matrix
-        temp_mat <- reshape2::acast(temp_df, y ~ x, value.var = "layer")
-        return(temp_mat)
-      })
-
-    # Replace
-    sub_list[which(sapply(sub_list, function(x) class(x) == "RasterLayer"))] <-
-      raster_list
-  }
-
-  # Round values to desired precision
-  sub_list<-
-    lapply(sub_list, function(x){
-      round_val * round(x / round_val)
-    })
-
-  # Pad matrices with NAs (because they are not actually adjacent in space)
-  sub_list <-
-    lapply(sub_list, function(x){
-      cbind(x, NA)
-    })
-
-  # Bind together
-  sub_mat <- do.call("cbind", sub_list)
-
-  return(list(sub_mat = sub_mat, n_mat = n_mat))
+        sub_list[which(sapply(sub_list, function(x) class(x) == "RasterLayer"))]
+    
+    if(length(raster_list) > 0){
+        raster_list <-
+            lapply(raster_list, function(rast){
+                # To dataframe first (seems to preserve coordinates more reliably)
+                temp_df <- raster::as.data.frame(rast, xy = TRUE)
+                # To matrix
+                temp_mat <- reshape2::acast(temp_df, y ~ x, value.var = "layer")
+                return(temp_mat)
+            })
+        
+        # Replace
+        sub_list[which(sapply(sub_list, function(x) class(x) == "RasterLayer"))] <-
+            raster_list
+    }
+    
+    # Round values to desired precision
+    sub_list<-
+        lapply(sub_list, function(x){
+            round_val * round(x / round_val)
+        })
+    
+    # Pad matrices with NAs (because they are not actually adjacent in space)
+    sub_list <-
+        lapply(sub_list, function(x){
+            cbind(x, NA)
+        })
+    
+    # Bind together
+    sub_mat <- do.call("cbind", sub_list)
+    
+    return(list(sub_mat = sub_mat, n_mat = n_mat))
 }
 
