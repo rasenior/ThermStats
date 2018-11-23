1eb0d0b5649fb33096e9cbbdc6f4091d4cb28bd1
Rebecca A. Senior <rebecca.a.senior@gmail.com>
2018-11-23 09:59:10 +0000

    Updating terminology & improving facetting where patches passed as a list (named by image id)
    Signed-off-by: Rebecca A. Senior <rebecca.a.senior@gmail.com>

--- R/plot_patches.R
+++ R/plot_patches.R
@@ -7,7 +7,9 @@
 #' or fortified dataframe (see \code{ggplot2::}\code{\link[ggplot2]{fortify}})
 #' with the variables 'long', 'lat' and 'group'.
 #' @param bg_colour Colour of the background polygon.
-#' @param facet Whether to plot facets by matrix_id. Defaults to FALSE.
+#' @param facet Whether to plot facets by image ID. Defaults to FALSE.
+#' @param facet_cols Number of facet columns. Defaults to NULL.
+#' @param facet_rows Number of facet rows. Defaults to NULL.
 #' @param plot_distribution Should a histogram be plotted? Defaults to TRUE.
 #' @param print_plot Should the resulting plots be printed? Defaults to FALSE.
 #' @param save_plot Should the resulting plots be saved? Defaults to TRUE.
@@ -98,6 +100,8 @@ plot_patches <- function(df,
                          bg_poly = NULL,
                          bg_colour = NULL,
                          facet = FALSE,
+                         facet_cols = NULL,
+                         facet_rows = NULL,
                          plot_distribution = TRUE,
                          print_plot = TRUE,
                          save_plot = FALSE,
@@ -120,227 +124,232 @@ plot_patches <- function(df,
                          patch_cols = c("mistyrose", "cornflowerblue"),
                          patch_labs = c("Hot spots", "Cold spots"),
                          val_lab = NULL) {
-
-  # Define function to capitalise first letter
-  simpleCap <- function(x) {
-    s <- strsplit(x, " ")[[1]]
-    paste(toupper(substring(s, 1,1)), substring(s, 2),
-          sep="", collapse=" ")
-  }
-
-  if(any(names(df) == "matrix_id")){
-
-    matrix_id_orig <- levels(df$matrix_id)
-    matrix_id <- vapply(matrix_id_orig, simpleCap, FUN.VALUE = character(1))
-
-    df$matrix_id <-
-      factor(df$matrix_id,
-             # Don't change levels
-             levels = matrix_id_orig,
-             # Do change labels
-             labels = matrix_id)
-  }
-
-  # Fix patch outline colours
-  names(patch_cols) <- patch_labs
-
-  if(is.null(val_lab)){
-    val_lab <- paste("Temperature (", "\U00B0", "C)",
-                     sep = "")
-  }
-
-  # Histogram --------------------------------------------------------------
-
-  if(plot_distribution){
-
-    message("Plotting distribution")
-
-    df$G_bin <- factor(df$G_bin, levels = c(0, 1, -1),
-                       labels = c("Background", "Hot spots", "Cold spots"))
-
-    p1 <-
-      ggplot() +
-      geom_histogram(data = df,
-                     aes(x = val, y = ..density..),
-                     colour = "black", fill = "white") +
-      geom_density(data = df,
-                   aes(x = val), alpha = 0.2, fill = "grey") +
-      xlab(val_lab) +
-      ylab("Density") +
-      theme_bw() +
-      theme(axis.title = element_text(size = lab_size),
-            axis.text = element_text(size = text_size),
-            panel.grid = element_blank(),
-            plot.margin = margin(0.1, 0.1, 0.1, 0.1, unit = "cm"))
-    if(facet){
-      p1 <- p1 + facet_wrap(~ matrix_id)
+    
+    # Define function to capitalise first letter
+    simpleCap <- function(x) {
+        s <- strsplit(x, " ")[[1]]
+        paste(toupper(substring(s, 1,1)), substring(s, 2),
+              sep="", collapse=" ")
     }
-
-  }else{
-    p1 <- NULL
-  }
-
-  # Image ----------------------------------------------------------
-
-  message("Plotting image with hot and cold spots")
-
-  if(facet){
-    # If facetting, need to apply this over multiple patch dfs
-    patches <-
-      lapply(patches, function(patches_i){
-        # Add comment attribute to correctly plot holes
-        for (x in 1:length(patches_i@polygons)) {
-          comment(patches_i@polygons[[x]]) =
-            rgeos::createPolygonsComment(patches_i@polygons[[x]])
+    
+    if(any(names(df) == "id")){
+        
+        img_id_orig <- unique(df$id)
+        img_id <- vapply(img_id_orig, simpleCap, FUN.VALUE = character(1))
+        
+        df$img_id <-
+            factor(df$id,
+                   # Don't change levels
+                   levels = img_id_orig,
+                   # Do change labels
+                   labels = img_id)
+    }
+    
+    # Fix patch outline colours
+    names(patch_cols) <- patch_labs
+    
+    if(is.null(val_lab)){
+        val_lab <- paste("Temperature (", "\U00B0", "C)",
+                         sep = "")
+    }
+    
+    # Histogram --------------------------------------------------------------
+    
+    if(plot_distribution){
+        
+        message("Plotting distribution")
+        
+        df$G_bin <- factor(df$G_bin, levels = c(0, 1, -1),
+                           labels = c("Background", "Hot spots", "Cold spots"))
+        
+        p1 <-
+            ggplot() +
+            geom_histogram(data = df,
+                           aes(x = val, y = ..density..),
+                           colour = "black", fill = "white") +
+            geom_density(data = df,
+                         aes(x = val), alpha = 0.2, fill = "grey") +
+            xlab(val_lab) +
+            ylab("Density") +
+            theme_bw() +
+            theme(axis.title = element_text(size = lab_size),
+                  axis.text = element_text(size = text_size),
+                  panel.grid = element_blank(),
+                  plot.margin = margin(0.1, 0.1, 0.1, 0.1, unit = "cm"))
+        if(facet){
+            p1 <- p1 + facet_wrap(~ img_id, nrow = facet_rows, ncol = facet_cols)
         }
-
-        # Fortify patch polygons to dataframe
-        patches_i <- fortify(patches_i, region = "layer")
-        return(patches_i)
-      })
-    patches <- do.call("rbind", patches)
-    # Add matrix ID
-    patches$matrix_id <- gsub("\\..*", "",row.names(patches))
-    row.names(patches) <- NULL
-    # Relevel
-    patches$matrix_id <-
-      factor(patches$matrix_id,
-             levels = matrix_id_orig,
-             labels = matrix_id)
-  }else{
-
-    # Add comment attribute to correctly plot holes
-    for (x in 1:length(patches@polygons)) {
-      comment(patches@polygons[[x]]) =
-        rgeos::createPolygonsComment(patches@polygons[[x]])
-    }
-
-    # Fortify patch polygons to dataframe
-    patches <- fortify(patches, region = "layer")
-  }
-
-  # Get rid of the background patch
-  patches <- patches[patches$id != 0, ]
-
-  # Assign patch category to hot or cold spot
-  patches$id <- factor(patches$id, levels = c(1, -1),
-                       labels = patch_labs)
-
-  # Create the plot
-  p2 <- ggplot() +
-    geom_raster(data = df,
-                aes(x = x, y = y, fill = val)) +
-    geom_polygon(data = patches,
-                 aes(y = lat,x = long, group = group, colour = id),
-                 alpha = 0, size = outline_size) +
-    theme_classic() +
-    theme(axis.line = element_blank(),
-          axis.title = element_blank(),
-          axis.text = element_blank(),
-          axis.ticks = element_blank(),
-          legend.box.spacing = unit(0,"cm"),
-          legend.position = "bottom",
-          legend.title = element_text(size = text_size),
-          legend.text = element_text(size = text_size),
-          legend.key = element_rect(fill = "black"),
-          plot.margin = margin(0.1, 0.1, 0, 0, unit = "cm")) +
-    scale_fill_gradientn(colours = val_pal,
-                         breaks = fill_breaks) +
-    scale_colour_manual(values = patch_cols, name = NULL) +
-    guides(fill = guide_colorbar(order = 1,
-                                 title = val_lab,
-                                 title.position = "top",
-                                 title.hjust = 0.5,
-                                 barheight = 0.8),
-           colour = guide_legend(order = 2,
-                                 keywidth = 0.7,
-                                 keyheight = 0.7,
-                                 direction = "vertical",
-                                 override.aes = list(alpha = 1,
-                                                     fill = "black",
-                                                     size = 0.8))) +
-    scale_y_continuous(expand = c(0, 0)) +
-    scale_x_continuous(expand = c(0,0))
-
-  # If background polygon is supplied:
-  if(!(is.null(bg_poly))){
-    p2 <- p2 +
-      geom_polygon(data = bg_poly,
-                   aes(x = long,
-                       y = lat,
-                       group = group),
-                   colour = bg_colour,
-                   alpha = 0)
-  }
-
-  # If facetting is required:
-  if(facet){
-    p2 <- p2 +
-      facet_wrap(~ matrix_id) +
-      theme(strip.background = element_blank())
-  }
-
-  # Printing/saving plots -----------------------------------------------------
-
-  if(isTRUE (print_plot)){
-
-    if (requireNamespace("gridExtra", quietly = TRUE)) {
-
-      if(plot_distribution){
-        suppressMessages(
-          gridExtra::grid.arrange(p1, p2, ncol=2)
-        )
-      } else{
-        print(p2)
-      }
-
-    } else {
-      suppressMessages(print(p1))
-      print(p2)
-    }
-  }
-
-  if(isTRUE (save_plot)){
-
-    # Define file names
-    if(is.null(file_name)){
-      p1_filename <-
-        file.path(out_dir, paste("distribution.",file_ext, sep = ""))
-      p2_filename <-
-        file.path(out_dir, paste("patches.",file_ext, sep = ""))
+        
     }else{
-
-      p1_filename <-
-        file.path(out_dir, paste(file_name,
-                                 "_distribution.",file_ext, sep = ""))
-      p2_filename <-
-        file.path(out_dir, paste(file_name, "_patches.",file_ext, sep = ""))
-
+        p1 <- NULL
     }
-
-    if(plot_distribution){
-      # Plot distribution
-      suppressMessages(
-        ggsave(plot = p1, filename = p1_filename,
+    
+    # Image ----------------------------------------------------------
+    
+    message("Plotting image with hot and cold spots")
+    
+    if(facet){
+        # If facetting, need to apply this over multiple patch dfs
+        patches <-
+            lapply(patches, function(patches_i){
+                
+                lyr_id <- names(patches_i)
+                names(patches_i) <- "layer"
+                
+                # Add comment attribute to correctly plot holes
+                for (x in 1:length(patches_i@polygons)) {
+                    comment(patches_i@polygons[[x]]) =
+                        rgeos::createPolygonsComment(patches_i@polygons[[x]])
+                }
+                
+                # Fortify patch polygons to dataframe
+                patches_i <- fortify(patches_i, region = "layer")
+                
+                # Add image ID
+                patches_i$img_id <- lyr_id
+                
+                return(patches_i)
+            })
+        patches <- do.call("rbind", patches)
+        # Relevel
+        patches$img_id <-
+            factor(patches$img_id,
+                   levels = img_id_orig,
+                   labels = img_id)
+    }else{
+        
+        # Add comment attribute to correctly plot holes
+        for (x in 1:length(patches@polygons)) {
+            comment(patches@polygons[[x]]) =
+                rgeos::createPolygonsComment(patches@polygons[[x]])
+        }
+        
+        # Fortify patch polygons to dataframe
+        patches <- fortify(patches, region = "layer")
+    }
+    
+    # Get rid of the background patch
+    patches <- patches[patches$id != 0, ]
+    
+    # Assign patch category to hot or cold spot
+    patches$id <- factor(patches$id, levels = c(1, -1),
+                         labels = patch_labs)
+    
+    # Create the plot
+    p2 <- ggplot() +
+        geom_raster(data = df,
+                    aes(x = x, y = y, fill = val)) +
+        geom_polygon(data = patches,
+                     aes(y = lat,x = long, group = group, colour = id),
+                     alpha = 0, size = outline_size) +
+        theme_classic() +
+        theme(axis.line = element_blank(),
+              axis.title = element_blank(),
+              axis.text = element_blank(),
+              axis.ticks = element_blank(),
+              legend.box.spacing = unit(0,"cm"),
+              legend.position = "bottom",
+              legend.title = element_text(size = text_size),
+              legend.text = element_text(size = text_size),
+              legend.key = element_rect(fill = "black"),
+              plot.margin = margin(0.1, 0.1, 0, 0, unit = "cm")) +
+        scale_fill_gradientn(colours = val_pal,
+                             breaks = fill_breaks) +
+        scale_colour_manual(values = patch_cols, name = NULL) +
+        guides(fill = guide_colorbar(order = 1,
+                                     title = val_lab,
+                                     title.position = "top",
+                                     title.hjust = 0.5,
+                                     barheight = 0.8),
+               colour = guide_legend(order = 2,
+                                     keywidth = 0.7,
+                                     keyheight = 0.7,
+                                     direction = "vertical",
+                                     override.aes = list(alpha = 1,
+                                                         fill = "black",
+                                                         size = 0.8))) +
+        scale_y_continuous(expand = c(0, 0)) +
+        scale_x_continuous(expand = c(0,0))
+    
+    # If background polygon is supplied:
+    if(!(is.null(bg_poly))){
+        p2 <- p2 +
+            geom_polygon(data = bg_poly,
+                         aes(x = long,
+                             y = lat,
+                             group = group),
+                         colour = bg_colour,
+                         alpha = 0)
+    }
+    
+    # If facetting is required:
+    if(facet){
+        p2 <- p2 +
+            facet_wrap(~ img_id, nrow = facet_rows, ncol = facet_cols)
+            theme(strip.background = element_blank())
+    }
+    
+    # Printing/saving plots -----------------------------------------------------
+    
+    if(isTRUE (print_plot)){
+        
+        if (requireNamespace("gridExtra", quietly = TRUE)) {
+            
+            if(plot_distribution){
+                suppressMessages(
+                    gridExtra::grid.arrange(p1, p2, ncol=2)
+                )
+            } else{
+                print(p2)
+            }
+            
+        } else {
+            suppressMessages(print(p1))
+            print(p2)
+        }
+    }
+    
+    if(isTRUE (save_plot)){
+        
+        # Define file names
+        if(is.null(file_name)){
+            p1_filename <-
+                file.path(out_dir, paste("distribution.",file_ext, sep = ""))
+            p2_filename <-
+                file.path(out_dir, paste("patches.",file_ext, sep = ""))
+        }else{
+            
+            p1_filename <-
+                file.path(out_dir, paste(file_name,
+                                         "_distribution.",file_ext, sep = ""))
+            p2_filename <-
+                file.path(out_dir, paste(file_name, "_patches.",file_ext, sep = ""))
+            
+        }
+        
+        if(plot_distribution){
+            # Plot distribution
+            suppressMessages(
+                ggsave(plot = p1, filename = p1_filename,
+                       dpi = 800, width = fig_width, height = fig_height, units = fig_units)
+            )
+        }
+        
+        # Plot image w patches
+        ggsave(plot = p2, filename = p2_filename,
                dpi = 800, width = fig_width, height = fig_height, units = fig_units)
-      )
+        
     }
-
-    # Plot image w patches
-    ggsave(plot = p2, filename = p2_filename,
-           dpi = 800, width = fig_width, height = fig_height, units = fig_units)
-
-  }
-
-  # Return plot objects if required
-  if(return_plot){
-    if(plot_distribution){
-      return(list(fig_distribution = p1, fig_patches = p2))
-    } else{
-      return(fig_patches = p2)
+    
+    # Return plot objects if required
+    if(return_plot){
+        if(plot_distribution){
+            return(list(fig_distribution = p1, fig_patches = p2))
+        } else{
+            return(fig_patches = p2)
+        }
+        
     }
-
-  }
-
-
+    
+    
 }
