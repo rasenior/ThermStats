e1095fa560e993cc96b6d618853918703e5101b2
Rebecca A. Senior <rebecca.a.senior@gmail.com>
2019-05-27 18:27:10 +0100

    Argument to pass path to exiftool installation and adding exiftool info to documentation
    Signed-off-by: Rebecca A. Senior <rebecca.a.senior@gmail.com>

--- R/batch_extract.R
+++ R/batch_extract.R
@@ -12,12 +12,29 @@
 #' all files are included.
 #' @param exc Vector of photo numbers to exclude. Defaults to NULL, meaning 
 #' all files are included.
+#' @param exiftoolpath Passed directly to 
+#' \code{Thermimage::}\code{\link[Thermimage]{readflirJPG}}: 
+#' "A character string that determines whether Exiftool has been 'installed' 
+#' (http://www.sno.phy.queensu.ca/~phil/exiftool/) or not. If Exiftool has been 
+#' installed in a specific location, use to direct to the folder location."
 #' @return A list containing:
 #'  \item{raw_dat}{A list with one element per input thermal image. Each element
 #'  is a numeric matrix of the raw infrared data.}
 #'  \item{camera_params}{A dataframe of callibration constants unique to each
 #'  camera.}
 #' @details Batch implementation of \code{Thermimage::}\code{\link[Thermimage]{readflirJPG}}.
+#' 
+#' Note that Exiftool is essential for this function to work correctly. See the
+#' documentation of \code{Thermimage::}\code{\link[Thermimage]{readflirJPG}}:
+#' 
+#' "Exiftool should install on most operating systems. Consult with 
+#' http://www.sno.phy.queensu.ca/~phil/exiftool/ for information on installing 
+#' Exiftool. If trouble installing, download Exiftool and set exiftoolpath to 
+#' the custom folder location. To test if the custom path to Exiftool will work 
+#' on your OS, try your own system or system2 call: 
+#' system2('/custompath/exiftool') to see if you get an error or not."
+#' @references
+#' Exiftool Command line tool: http://www.sno.phy.queensu.ca/~phil/exiftool/
 #' @examples
 #' # Batch extract four FLIR thermal images included in this package.
 #' results <-
@@ -30,7 +47,8 @@ batch_extract <- function(in_dir,
                           out_dir = NULL,
                           file_name = NULL,
                           inc = NULL,
-                          exc = NULL){
+                          exc = NULL,
+                          exiftoolpath = "installed"){
 
   # File names --------------------------------------------------------------
 
@@ -61,16 +79,17 @@ batch_extract <- function(in_dir,
 
   # Extract FLIR data -------------------------------------------------------
 
-  for(i in 1:length(file.names)){
+  for (i in 1:length(file.names)) {
     cat("Processing file", i, "of", length(file.names),"\n")
     cat("Reading file...","\n")
 
     # Try and read in each FLIR file
-    photo_i<-tryCatch(
+    photo_i <- tryCatch(
       {
-        Thermimage::readflirJPG(imagefile = file.names[i], exiftoolpath = "installed")
+        Thermimage::readflirJPG(imagefile = file.names[i], 
+                                exiftoolpath = exiftoolpath)
       },
-      error=function(x){
+      error = function(x){
         message(paste("Couldn't process file:",file.names[i]))
         return(NA)
       })
@@ -78,7 +97,7 @@ batch_extract <- function(in_dir,
 
     # Flip the matrix (makes plotting later on easier)
     photo_i <- Thermimage::mirror.matrix(Thermimage::rotate180.matrix(photo_i))
-    colnames(photo_i)<-NULL
+    colnames(photo_i) <- NULL
 
     # Write the matrix to the appropriate index in the empty list
     raw_dat[[i]] <- photo_i
@@ -88,7 +107,7 @@ batch_extract <- function(in_dir,
   cat("Extracting camera parameters...","\n")
   camera_params <-
     Thermimage::flirsettings(imagefile = file.names[1],
-                             exiftoolpath = "installed")
+                             exiftoolpath = exiftoolpath)
 
   # Reduce to parameters of interest
   camera_params <- data.frame(camera_params)
@@ -99,10 +118,11 @@ batch_extract <- function(in_dir,
 
   results <- list(raw_dat = raw_dat, camera_params = camera_params)
 
-  if(write_results){
+  if (write_results) {
 
-    if(is.null(out_dir)) out_dir <- getwd()
-    if(is.null(file_name)) file_name <- paste("flir_raw_", Sys.Date(),".Rdata",sep="")
+    if (is.null(out_dir)) out_dir <- getwd()
+    if (is.null(file_name)) file_name <- paste("flir_raw_", Sys.Date(),
+                                               ".Rdata",sep = "")
 
     out_path <- file.path(out_dir, paste(file_name, ".Rdata", sep = ""))
     save(results,file = out_path)
