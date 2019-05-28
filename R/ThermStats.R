b4deec4324aab7d00532b0ab0653974d3e2fba7f
Rebecca A. Senior <rebecca.a.senior@gmail.com>
2019-05-28 09:58:22 +0100

    Adding documentation
    Signed-off-by: Rebecca A. Senior <rebecca.a.senior@gmail.com>

---
+++ R/ThermStats.R
@@ -0,0 +1,13 @@
+#' \code{ThermStats} package
+#'
+#' Calculate statistics for thermal images
+#'
+#' See the README on
+#' \href{https://github.com/rasenior/ThermStats#readme}{GitHub}
+#'
+#' @docType package
+#' @name ThermStats
+NULL
+
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+if (getRversion() >= "2.15.1")  utils::globalVariables(c("."))
---
+++ R/sulawesi_bg.R
@@ -0,0 +1,12 @@
+#' Boundary polygon of Sulawesi
+#'
+#' Boundary polygon of Sulawesi for plotting with Worldclim2 raster data 
+#' \code{sulawesi_temp}.
+#'
+#' @docType data
+#' @usage sulawesi_bg
+#' @format A SpatialPolygonsDataFrame
+#' @keywords datasets
+#' @examples
+#' sp::plot(sulawesi_bg)
+"sulawesi_bg"
