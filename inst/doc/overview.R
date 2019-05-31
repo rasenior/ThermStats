2dd8191531696120464ecf44640ccee33c2b5046
Rebecca A. Senior <rebecca.a.senior@gmail.com>
2019-05-31 09:20:35 +0100

    Edit to vignette so slow code chunks are not run
    Signed-off-by: Rebecca A. Senior <rebecca.a.senior@gmail.com>

--- inst/doc/overview.R
+++ inst/doc/overview.R
@@ -1,9 +1,8 @@
 ## ----setup, include=FALSE------------------------------------------------
 knitr::opts_chunk$set(echo = FALSE, message = FALSE, warning = FALSE,
-                      cache = TRUE,
                       fig.align = "centre", fig.width = 16.6/2.54)
 
-## ----fig-B-1, fig.cap= "Figure 1: Schematic summarising the key functions for processing groups of images (left) or a single image (right).", fig.pos = 'H', out.width = "400px"----
+## ----fig-1, fig.cap= "Figure 1: Schematic summarising the key functions for processing groups of images (left) or a single image (right).", fig.pos = 'H', out.width = "400px"----
 knitr::include_graphics("schematic.png")
 
 ## ----install, echo = TRUE, eval = FALSE----------------------------------
@@ -12,14 +11,14 @@ knitr::include_graphics("schematic.png")
 ## ----load, echo = TRUE---------------------------------------------------
 library(ThermStats)
 
-## ----batch-extract, echo= TRUE, results = "hide"-------------------------
-# Batch extract thermal images included in ThermStats installation
-flir_raw <-
-    batch_extract(in_dir = system.file("extdata", 
-                                       package = "ThermStats"),
-                  write_results = FALSE)
+## ----batch-extract, echo= TRUE, eval = FALSE-----------------------------
+#  # Batch extract thermal images included in ThermStats installation
+#  flir_raw <-
+#      batch_extract(in_dir = system.file("extdata",
+#                                         package = "ThermStats"),
+#                    write_results = FALSE)
 
-## ----batch-convert, echo= TRUE, results = "hide"-------------------------
+## ----batch-convert, echo = TRUE, results = "hide"------------------------
 # Define raw data
 raw_dat <- flir_raw$raw_dat
 # Define camera calibration constants dataframe
@@ -53,30 +52,30 @@ flir_converted <-
         # Whether to write results or just return
         write_results = FALSE)
 
-## ----get-stats, echo= TRUE, results = "hide"-----------------------------
-flir_stats <-
-    get_stats( 
-        # The temperature dataset
-        img = flir_converted$`8565`,
-        # The ID of the dataset
-        id = "8565",
-        # Whether or not to calculate thermal connectivity
-        calc_connectivity = FALSE,
-        # Whether or not to identify hot and cold spots
-        patches = TRUE,  
-        # The image projection (only relevant for geographic data)
-        img_proj = NULL,
-        # The image extent (only relevant for geographic data)
-        img_extent = NULL, 
-        # The data to return
-        return_vals = c("df", # Temperature data as dataframe
-                        "patches", # Patch outlines
-                        "pstats"), # Patch statistics dataframe
-        # The summary statistics of interest
-        sum_stats = c("median", "SHDI",
-                      "perc_5", "perc_95"))
+## ----get-stats, echo= TRUE, eval = FALSE---------------------------------
+#  flir_stats <-
+#      get_stats(
+#          # The temperature dataset
+#          img = flir_converted$`8565`,
+#          # The ID of the dataset
+#          id = "8565",
+#          # Whether or not to calculate thermal connectivity
+#          calc_connectivity = FALSE,
+#          # Whether or not to identify hot and cold spots
+#          patches = TRUE,
+#          # The image projection (only relevant for geographic data)
+#          img_proj = NULL,
+#          # The image extent (only relevant for geographic data)
+#          img_extent = NULL,
+#          # The data to return
+#          return_vals = c("df", # Temperature data as dataframe
+#                          "patches", # Patch outlines
+#                          "pstats"), # Patch statistics dataframe
+#          # The summary statistics of interest
+#          sum_stats = c("median", "SHDI",
+#                        "perc_5", "perc_95"))
 
-## ----tab-B-1, results='asis'---------------------------------------------
+## ----tab-1, results='asis'-----------------------------------------------
 tab1 <- "
 Table: Table 1: Example metadata denoting the grouping ('rep_id') of different thermal images. Statistics can be calculated over multiple images within a group, using the function `stats_by_group`.
 
@@ -88,30 +87,30 @@ Table: Table 1: Example metadata denoting the grouping ('rep_id') of different t
 |     8613|T7P2   |    23.50|           96|
 "
 
-cat(tab1) # output the table in a format good for HTML/PDF/docx conversion
+cat(tab1)
 
-## ----stats-by-group, results = "hide"------------------------------------
-flir_stats_group <-
-    stats_by_group(
-        # A dataframe denoting the grouping
-        metadata = metadata,  
-        # List of images
-        img_list = flir_converted,
-        # Variable denoting the ID of unique images
-        idvar = "photo_no",
-        # Variable denoting the grouping
-        grouping_var = "rep_id",
-        # Desired precision of data
-        round_val = 0.5,
-        # The data to return
-        return_vals = c("df", # Temperature data as dataframe
-                        "patches", # Patch outlines
-                        "pstats"), # Patch statistics dataframe
-        # The summary statistics of interest
-        sum_stats = c("median", "SHDI",
-                      "perc_5", "perc_95"))
+## ----stats-by-group, eval = FALSE----------------------------------------
+#  flir_stats_group <-
+#      stats_by_group(
+#          # A dataframe denoting the grouping
+#          metadata = metadata,
+#          # List of images
+#          img_list = flir_converted,
+#          # Variable denoting the ID of unique images
+#          idvar = "photo_no",
+#          # Variable denoting the grouping
+#          grouping_var = "rep_id",
+#          # Desired precision of data
+#          round_val = 0.5,
+#          # The data to return
+#          return_vals = c("df", # Temperature data as dataframe
+#                          "patches", # Patch outlines
+#                          "pstats"), # Patch statistics dataframe
+#          # The summary statistics of interest
+#          sum_stats = c("median", "SHDI",
+#                        "perc_5", "perc_95"))
 
-## ----tab-B-2, results='asis'---------------------------------------------
+## ----tab-2, results='asis'-----------------------------------------------
 tab2 <- "
 Table: Table 2: A snippet of hot spot patch statistics returned by `stats_by_group`, which implements `get_stats` within groups.
 
@@ -121,9 +120,9 @@ Table: Table 2: A snippet of hot spot patch statistics returned by `stats_by_gro
 |       24.0|         23|        25.0|         1.68|            7.80|           0.855|
 "
 
-cat(tab2) # output the table in a format good for HTML/PDF/docx conversion
+cat(tab2)
 
-## ----fig-B-2, fig.cap= "Figure 2: The output of `plot_patches` includes a histogram and the original temperature data overlaid with outlines of hot and cold spots, identified using the G* variant of the Getis-Ord local statistic.", echo = TRUE----
+## ----fig-2, fig.cap= "Figure 2: The output of `plot_patches` includes a histogram and the original temperature data overlaid with outlines of hot and cold spots, identified using the G* variant of the Getis-Ord local statistic.", echo = TRUE----
 plot_patches(
     # The raw temperature data
     df = flir_stats$df,
